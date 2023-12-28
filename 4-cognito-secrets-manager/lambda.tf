7140ed09c236a1018ed1d377b576ff862ff15e2c
Khalid Alkhalili <kfkhalili@hotmail.com>
2023-12-28 23:25:53 +0400

    Add Lambda functions for Cognito (register/login).

---
+++ 4-cognito-secrets-manager/lambda.tf
@@ -0,0 +1,75 @@
+#### COGNITO LAMBDAS ####
+resource "aws_lambda_function" "register_user_lambda" {
+  function_name = "${var.app_name}_registerUserCognito"
+  role          = aws_iam_role.lambda_cognito_role.arn
+
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  filename      = "./lambda_registerUserCognito.zip"
+  source_code_hash = filebase64sha256("./lambda_registerUserCognito.zip")
+
+
+  environment {
+    variables = {
+      COGNITO_CLIENT_ID = aws_cognito_user_pool_client.user_pool_client.id
+    }
+  }
+}
+
+resource "aws_lambda_function" "login_user_lambda" {
+  function_name = "${var.app_name}_loginUserCognito"
+  role          = aws_iam_role.lambda_cognito_role.arn
+
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  filename      = "./lambda_loginUserCognito.zip"
+  source_code_hash = filebase64sha256("./lambda_loginUserCognito.zip")
+
+  environment {
+    variables = {
+      COGNITO_CLIENT_ID = aws_cognito_user_pool_client.user_pool_client.id
+    }
+  }
+}
+
+#### IAM ROLES ####
+resource "aws_iam_role" "lambda_cognito_role" {
+  name = "${var.app_name}_lambda_cognito_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "lambda_cognito_policy" {
+  name        = "${var.app_name}_lambda_cognito_policy"
+  description = "IAM policy for Lambda function to interact with Cognito"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "cognito-idp:SignUp",
+          "cognito-idp:InitiateAuth"
+        ],
+        Effect = "Allow",
+        Resource = "*"
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_cognito_attach" {
+  role       = aws_iam_role.lambda_cognito_role.name
+  policy_arn = aws_iam_policy.lambda_cognito_policy.arn
+}
