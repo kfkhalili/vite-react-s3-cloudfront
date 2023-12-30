11e37343716087859a31c47b65bc83435603eafe
Khalid Alkhalili <kfkhalili@hotmail.com>
2023-12-30 18:26:04 +0100

    Include required flows.

--- 4-cognito-secrets-manager/cognito.tf
+++ 4-cognito-secrets-manager/cognito.tf
@@ -8,6 +8,11 @@ resource "aws_cognito_user_pool" "user_pool" {
 resource "aws_cognito_user_pool_client" "user_pool_client" {
   name         = "${var.app_name}-user-pool-client"
   user_pool_id = aws_cognito_user_pool.user_pool.id
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
 }
 
 # Outputs
